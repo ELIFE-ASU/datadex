2dae608623bc159b7a62ef1518216b25abfa1a24
Douglas Moore <doug@dglmoore.com>
2017-06-16 15:40:27 -0700

    Implement indexing

--- datadex.py
+++ datadex.py
@@ -3,31 +3,84 @@
 # license that can be found in the LICENSE file.
 import os
 import os.path as path
+import re
 import sqlite3
+import hashlib
+
+def parse_to(value, cls):
+    """
+    Parse a value to a new class.
+    """
+    parsed = False
+    try:
+        value = cls(value)
+        parsed = True
+    except ValueError:
+        pass
+    return value, parsed
+
+def parse(value):
+    """
+    Parse a value into one of the sqlite type.
+    """
+    types = [int, float]
+    for new_type in types:
+        value, parsed = parse_to(value, new_type)
+        if parsed:
+            break
+    return value
+
+def hash_directory(root_dir, hasher=hashlib.sha256(), top_level=True):
+    """
+    Hash a directory structure.
+    """
+    if top_level:
+        cwd = os.getcwd()
+        os.chdir(root_dir)
+        root_dir = "."
+    for root, directories, filenames in os.walk(root_dir):
+        for filename in filenames:
+            filepath = path.normpath(path.join(root, filename))
+            hasher.update(filepath.encode())
+            with open(filepath, 'rb') as handle:
+                hasher.update(handle.read())
+        for directory in directories:
+            hash_directory(path.join(root, directory), hasher=hasher, top_level=False)
+    if top_level:
+        os.chdir(cwd)
+    return hasher
 
 class DataDex(object):
     """
     A DataDex is a database mapping parameters to filenames.
     """
-    def __init__(self, dex):
+    def __init__(self, dex=None, verbose=False, hash_dir=False):
         """
         Initialize the DataDex with a database file.
         :param dex: a path to a sqlite3 database
         """
-        if path.exists(dex) and not path.isfile(dex):
-            raise ValueError("dex is not a database file")
-        elif not path.exists(dex):
-            try:
-                os.makedirs(path.dirname(dex), exist_ok=True)
-            except OSError:
-                pass
+        if dex is None:
+            dex = "dex.db"
+        else:
+            if path.exists(dex) and not path.isfile(dex):
+                raise ValueError("dex is not a database file")
+            elif not path.exists(dex):
+                try:
+                    os.makedirs(path.dirname(dex), exist_ok=True)
+                except OSError:
+                    pass
         self.__database = dex
+        self.verbose = verbose
+        self.hash_dir = hash_dir
+
         self.__conn = None
         self.connect()
 
         self.__headers = self.get_headers()
         if self.headers is not None and self.headers[-1].lower() != 'filename':
             raise RuntimeError("filename column is missing from library")
+
+        self.params_filenames = ["params.dex", "params.txt", "params.dat"]
 
     def __dealloc__(self):
         """
@@ -61,20 +114,46 @@ class DataDex(object):
         """
         Create a library with the provided parameter names
         """
-        column_names = list(map(lambda x: x.lower(), column_names))
-        column_names.append("filename")
+        if len(column_names) == 0:
+            column_names = self.__parse_headers()
+        else:
+            column_names = list(map(lambda x: x.lower(), column_names))
+
+        if "filename" not in column_names:
+            column_names.append("filename")
+
         if self.headers is None:
-            cursor, disconnect_after = self.get_cursor()
+            cursor, _ = self.get_cursor()
             column_headers = '({})'.format(','.join(column_names))
             query = "CREATE TABLE IF NOT EXISTS LIBRARY" + column_headers
             cursor.execute(query)
             self.commit()
             self.__headers = column_names
-            if disconnect_after:
-                self.disconnect()
         elif self.headers != column_names:
             msg = 'library already exists with column names {}'
             raise RuntimeError(msg.format(column_headers[:-1]))
+
+    def drop_library(self):
+        """
+        Drop the library from the database.
+        """
+        cursor, _ = self.get_cursor()
+        cursor.execute("DROP TABLE IF EXISTS LIBRARY")
+        self.commit()
+
+    def reset_library(self):
+        """
+        Reset the library to the empty state.
+        """
+        try:
+            self.drop_library()
+        except sqlite3.OperationalError:
+            pass
+        cursor, _ = self.get_cursor()
+        column_headers = '({})'.format(','.join(self.headers))
+        query = "CREATE TABLE IF NOT EXISTS LIBRARY" + column_headers
+        cursor.execute(query)
+        self.commit()
 
     @property
     def headers(self):
@@ -90,14 +169,12 @@ class DataDex(object):
         Get the headers from the library.
         """
         headers = None
-        cursor, disconnect_after = self.get_cursor()
+        cursor, _ = self.get_cursor()
         try:
             cursor.execute("SELECT * FROM LIBRARY")
             headers = list(map(lambda x: x[0], cursor.description))
         except sqlite3.OperationalError:
             pass
-        if disconnect_after:
-            self.disconnect()
         return headers
 
     def get_cursor(self):
@@ -117,29 +194,183 @@ class DataDex(object):
         if self.is_connected():
             self.__conn.commit()
 
+    def query(self, query):
+        """
+        Run an arbitrary SQLite query.
+        """
+        cursor, _ = self.get_cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+
     def search(self, *conditions):
         """
         Query the database, returning any files that match
         """
-        query = "SELECT FILENAME FROM LIBRARY WHERE " + " AND ".join(conditions)
-        print(query)
-        cursor, disconnect_after = self.get_cursor()
-        cursor.execute(query)
-        filenames = list(map(lambda x: x[0], cursor.fetchall()))
-        if disconnect_after:
-            self.disconnect()
-        return filenames
+        query = "SELECT FILENAME FROM LIBRARY"
+        if len(conditions) != 0:
+            query += " WHERE " + " AND ".join(conditions)
+        raw_response = self.query(query)
+        return list(map(lambda x: path.normpath(x[0]), raw_response))
 
-    def add(self, entry):
+    def lookup(self, entry=None, ignore_filename=True, enforce_null=True):
+        """
+        Look for an exact entry in the database
+        """
+        query = "SELECT * FROM LIBRARY"
+        if entry is not None and len(query) != 0:
+            conditions = []
+            for field in entry:
+                if ignore_filename and field.lower() == "filename":
+                    continue
+                value = entry[field]
+                conditions.append("{} IS {}".format(field.upper(), repr(value)))
+            if enforce_null:
+                for field in self.headers:
+                    if field.lower() == "filename" and ignore_filename:
+                        continue
+                    elif field not in entry:
+                        conditions.append("{} IS NULL".format(field.upper()))
+            query += " WHERE " + " AND ".join(conditions)
+        return self.query(query)
+
+    def add(self, entry, ignore_filename=True, enforce_null=True):
         """
         Add a row to the database
         """
-        values = entry.values()
-        fields = "({})".format(", ".join(map(lambda x: x.upper(), entry.keys())))
-        values = "({})".format(", ".join(map(repr, entry.values())))
-        query = "INSERT INTO LIBRARY {} values {}".format(fields, values)
-        print(query)
-        cursor, disconnect_after = self.get_cursor()
-        cursor.execute(query)
-        if disconnect_after:
-            self.disconnect()
+        if len(self.lookup(entry, ignore_filename, enforce_null)) == 0:
+            values = entry.values()
+            fields = "({})".format(", ".join(map(lambda x: x.upper(), entry.keys())))
+            values = "({})".format(", ".join(map(repr, entry.values())))
+            self.query("INSERT INTO LIBRARY {} values {}".format(fields, values))
+            return True
+        return False
+
+    def add_dir(self, dirname, ignore_filename=True, enforce_null=True):
+        """
+        Add a directory to the index.
+        """
+        if not path.exists(dirname):
+            raise RuntimeError('"{}" does not exist'.format(dirname))
+        elif not path.isdir(dirname):
+            raise RuntimeError('"{}" is not a directory'.format(dirname))
+
+        param_files = self.__get_param_files(dirname)
+        if len(param_files) == 0:
+            return False, False
+
+        if self.hash_dir:
+            name = hash_directory(dirname).hexdigest()
+            name = path.join(path.dirname(dirname), name)
+            if path.exists(name) and len(self.lookup({"filename": name}, False, False)) != 0:
+                return True, False
+        else:
+            name = path.normpath(dirname)
+
+        file_found, file_added = True, False
+        for param_filepath in param_files:
+            params = DataDex.__parse_params(param_filepath)
+            if len(params) != 0:
+                params["filename"] = name
+                added = self.add(params, ignore_filename, enforce_null)
+                file_added = file_added or added
+            else:
+                msg = 'empty params file found {}'
+                raise RuntimeError(msg.format(param_filepath))
+
+        if (file_added or (file_found and not path.exists(name))) and self.hash_dir:
+            os.rename(dirname, name)
+
+        return file_found, file_added
+
+    def index(self, root_dir=".", ignore_filename=True, enforce_null=True):
+        """
+        Index a directory
+        """
+        something_was_indexed = False
+        for root, dirs, _ in os.walk(root_dir):
+            for directory in dirs:
+                dirname = os.path.join(root, directory)
+                found, added = self.add_dir(dirname, ignore_filename, enforce_null)
+                if self.verbose:
+                    if found and added:
+                        status = "indexed"
+                    elif found:
+                        status = "already indexed"
+                    else:
+                        status = "skipped (no params file)"
+                    print("* Directory {} {}".format(dirname, status))
+                something_was_indexed = something_was_indexed or added
+        return something_was_indexed
+
+    def reindex(self, root_dir=".", ignore_filename=True, enforce_null=True):
+        """
+        Reset the library and index a directory
+        """
+        self.reset_library()
+        self.index(root_dir, ignore_filename, enforce_null)
+
+    def prune(self):
+        """
+        Remove nonexistent datasets from the library.
+        """
+        for dataset in self.search():
+            if not path.exists(dataset):
+                query = 'DELETE FROM LIBRARY WHERE FILENAME = {}'
+                self.query(query.format(repr(dataset)))
+
+    def __parse_headers(self):
+        """
+        Parse a header file
+        """
+        column_names = []
+        for params_filename in self.params_filenames:
+            if path.exists(params_filename) and path.isfile(params_filename):
+                comment_regex = re.compile("^\\s*#.*$")
+                header_regex = re.compile("^\\s*(\\S+)\\s*$")
+                with open(params_filename, 'r') as handle:
+                    for (line_number, line) in enumerate(handle):
+                        if not comment_regex.match(line):
+                            match = header_regex.match(line)
+                            if match:
+                                header = match.group(1)
+                                if header not in column_names:
+                                    column_names.append(header)
+                            else:
+                                msg = "invalid header entry, line {}"
+                                raise RuntimeError(msg.format(line_number))
+        if len(column_names) == 0:
+            raise RuntimeError("cannot create library; no headings provided")
+
+        return column_names
+
+    @staticmethod
+    def __parse_params(filename):
+        """
+        Parse a parameters file.
+        """
+        params = dict()
+        comment_regex = re.compile("^\\s*#.*$")
+        entry_regex = re.compile("^\\s*(\\S+)\\s*[:=]\\s*(\\S+)$")
+        with open(filename, 'r') as handle:
+            for (line_number, line) in enumerate(handle):
+                if not comment_regex.match(line):
+                    match = entry_regex.match(line)
+                    if match:
+                        field = match.group(1)
+                        value = parse(match.group(2))
+                        params[field] = value
+                    else:
+                        msg = "invalid parameter entry, line {}"
+                        raise RuntimeError(msg.format(line_number))
+        return params
+
+    def __get_param_files(self, dirname):
+        """
+        Find all parameter files in a directory
+        """
+        files = []
+        for filename in self.params_filenames:
+            filepath = path.normpath(path.join(dirname, filename))
+            if path.exists(filepath) and path.isfile(filepath):
+                files.append(filepath)
+        return files
