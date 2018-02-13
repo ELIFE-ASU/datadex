f4c2070640a6b9dbfc542444d04b7873b518eb05
Douglas G. Moore <doug@dglmoore.com>
2018-02-13 14:52:04 -0700

    Fix unicode support under python 3

--- datadex/datadex.py
+++ datadex/datadex.py
@@ -1,6 +1,7 @@
 # Copyright 2017 Douglas G. Moore, Harrison Smith. All rights reserved.
 # Use of this source code is governed by a MIT
 # license that can be found in the LICENSE file.
+from .python3 import *
 import os
 import os.path as path
 import re
---
+++ datadex/python3.py
@@ -0,0 +1,8 @@
+# Copyright 2018 Douglas G. Moore, Harrison Smith. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+import sys
+if sys.version_info >= (3,):
+    long = int
+    unicode = str
+
